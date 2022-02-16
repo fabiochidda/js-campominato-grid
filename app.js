2a0cf7d534ec6095f34c6169e1b4b4bcaae6e778
fabiochidda <fabio.chidda99@gmail.com>
2022-02-17 00:04:04 +0100

    Modifiche nell'html, aggiunta la classe per le dimensioni delle celle e creata la funzione per selezionare la difficoltà e creare le celle

---
+++ app.js
@@ -0,0 +1,10 @@
+const startGameButton = document.getElementById("startGame");
+const difficultySelector = document.getElementById("gameMode");
+
+const startGameFunction  = () => {
+
+    console.log("test")
+
+}
+
+startGameButton.addEventListener("click", startGameFunction)
