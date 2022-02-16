a0476ba404257a6ab589969e8088bcbd621257b9
fabiochidda <fabio.chidda99@gmail.com>
2022-02-17 00:13:34 +0100

    Creata la funzione per generare righe e colonne

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const startGameButton = document.getElementById("startGame");
 const difficultySelector = document.getElementById("gameMode");
 
+
+
 const startGameFunction  = () => {
 
-    console.log("test")
+    console.log(difficultySelector.value)
+
+    let cellRows;
+    let cellColumns;
+
+    if (difficultySelector.value == 1) {
+
+        console.log("Hai selezionato la modalità Facile")
+        cellRows = 10;
+        cellColumns = 10;
+        
+    } else if (difficultySelector.value == 2) {
+
+        console.log("Hai selezionato la modalità Media")
+        cellRows = 9;
+        cellColumns = 9;
+        
+    } else {
+
+        console.log("Hai selezionato la modalità Difficile")
+        cellRows = 7;
+        cellColumns = 7;
+        
+    }
+    
+    const gameCells = cellRows * cellColumns;
+
+    console.log(gameCells)
 
 }
 
