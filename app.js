545f96f63c4ed927c4ce774c693ad73e925b853a
fabiochidda <fabio.chidda99@gmail.com>
2022-02-17 00:33:57 +0100

    Aggiunto il ciclo per generare le celle all'interno del dom con gli stili appropriati in base alla difficoltà

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const startGameButton = document.getElementById("startGame");
 const difficultySelector = document.getElementById("gameMode");
+const gameContainer = document.querySelector(".ms_game_container");
 
 
 
@@ -33,6 +34,29 @@ const startGameFunction  = () => {
     const gameCells = cellRows * cellColumns;
 
     console.log(gameCells)
+
+    for (let i=0; i < gameCells; i++) {
+
+        const gameCellSquare = document.createElement("div")
+        gameCellSquare.classList.add("ms_gameCell")
+        gameCellSquare.append(i + 1)
+        gameContainer.append(gameCellSquare)
+
+        if (gameCells == 100) {
+
+            gameCellSquare.classList.add("ms_easy")
+
+        } else if (gameCells == 81) {
+
+            gameCellSquare.classList.add("ms_medium")
+
+        } else {
+
+            gameCellSquare.classList.add("ms_hard")
+            
+        }
+        
+    }
 
 }
 
